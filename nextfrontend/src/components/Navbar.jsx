b5f0077a1d6970cfbf778059e487a21a19ea206d
Vikky94 <88605615+Vikky94@users.noreply.github.com>
2025-01-17 20:28:06 +0530

    Adding Initial Header

---
+++ nextfrontend/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+"use client"
+import Link from "next/link"
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+
+
+export function NavigationMenuDemo() {
+    const data = [
+        {
+            title:"Register",
+            href:"/register"
+        },
+        {
+            title:"Login",
+            href:"/login"
+        }
+    ]
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {
+            data.map((ele,i) => {
+                return (
+                    <NavigationMenuItem key={i}>
+                    <Link href={ele.href} legacyBehavior passHref>
+                      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        {ele.title}
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                );
+            })
+        }
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
